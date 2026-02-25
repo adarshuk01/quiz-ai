84c7344559db9ec05999673f7dbcc69524df930e
Adarsh uk <adarshuk79@gmail.com>
2026-02-25 10:08:22 +0530

    changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,26 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected");
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
+  if (cached.conn) {
+    return cached.conn;
   }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(process.env.MONGO_URI, {
+        bufferCommands: false,
+      })
+      .then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 module.exports = connectDB;
